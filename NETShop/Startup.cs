7088f25f1be8bfcdcf191610b7dd14984f8dca79
Marius Riis Haugan <hauganmarius@outlook.com>
2018-06-17 14:58:16 +0200

    Initial startup, and view imports.

--- NETShop/Startup.cs
+++ NETShop/Startup.cs
@@ -15,6 +15,7 @@ namespace NETShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(); // Set up shared objects for MVC.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,12 @@ namespace NETShop
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStatusCodePages(); // Add HTML to HTTP responses normally without body (e.g. 404 - Not Found).
+            app.UseStaticFiles(); // Enable support for serving files from wwwroot folder.
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+
             });
         }
     }
