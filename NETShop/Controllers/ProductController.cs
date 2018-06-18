4ebd87952cbdb228a762a9dfde05f94180c62687
Marius Riis Haugan <hauganmarius@outlook.com>
2018-06-18 05:59:27 +0200

    Initial product controller; list action.

---
+++ NETShop/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETShop.Models.Repositories.Interfaces;
+
+namespace NETShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List => View(repository.Products);
+    }
+}
